62afc951e38975c7b5bcb58c40fc25811ea243a0
PolinaSloboda <sloboda.polina@gmail.com>
2019-10-07 10:36:04 +0300

    little bit refactoring - moved getCityByGeolocation to cityAdder

--- src/cityAdder.js
+++ src/cityAdder.js
@@ -32,7 +32,29 @@ export function addCityByName(
     }
 }
 
-export function addCityByCoords(lat, lon, dispatch) {
+export function addCityByGeolocation(dispatch) {
+    var addDefaultCity = () =>
+        addCityByName(
+            "Sankt-Peterburg", //this is the default value,
+            'SET_GEOLOCATION',
+            dispatch
+        );
+
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            position => addCityByCoords(
+                position.coords.latitude,
+                position.coords.longitude,
+                dispatch
+            ),
+            addDefaultCity
+        );
+    } else {
+        addDefaultCity();
+    }
+}
+
+function addCityByCoords(lat, lon, dispatch) {
     console.log(lat, lon);
     var city = {"coord":{"lon":145.77,"lat":-16.92},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":300.15,"pressure":1007,"humidity":74,"temp_min":300.15,"temp_max":300.15},"visibility":10000,"wind":{"speed":3.6,"deg":160},"clouds":{"all":40},"dt":1485790200,"sys":{"type":1,"id":8166,"message":0.2064,"country":"AU","sunrise":1485720272,"sunset":1485766550},"id":2172797,"name":"London","cod":200};
     dispatch({
@@ -55,4 +77,4 @@ function preprocessData(data) {
     return data;
 }
 
-export default {addCityByName, addCityByCoords};
+export default {addCityByName, addCityByGeolocation};
--- src/components/GeorefRefresh.js
+++ src/components/GeorefRefresh.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as cityAdder from '../cityAdder';
+import { addCityByGeolocation } from '../cityAdder';
 
 class GeorefRefresh extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSearch = this.handleSearch.bind(this);
-    }
-
     render() {
         return (
-            <button className="georefRefresh" onClick={this.handleSearch}>Обновить геолокацию</button>
+            <button className="georefRefresh"
+                onClick={() => addCityByGeolocation(this.props.dispatch)}>
+                    Обновить геолокацию
+            </button>
         );
-    }
-
-    handleSearch(event) {
-        var addDefaultCity = () =>
-            cityAdder.addCityByName(
-                "Sankt-Peterburg", //this is the default value,
-                'SET_GEOLOCATION',
-                this.props.dispatch
-            );
-
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                position => cityAdder.addCityByCoords(position.coords.latitude, position.coords.longitude, this.props.dispatch),
-                addDefaultCity
-            );
-        } else {
-            addDefaultCity();
-        }
     }
 }
 
